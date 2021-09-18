a7901e5f2e020dfa73eb51a4eeefa020675b2a3b
saquib1401 <saquib1401@gmail.com>
2021-09-18 10:56:12 +0530

    add counter files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+
 //import  Form from "./components/Form";
 //import Clock from "./components/Clock";
-import Cards from "./components/Cards";
+//import Cards from "./components/Cards";
 //import RenderList from "./components/RenderList";
 //import RenderListSearch from "./components/RenderListSearch";
 
 //import Count from "./components/Count"
+//import BasicTwo from "./Hook/Basic";
+//import BasicOne from "./Hook/BasicOne";
+
+import Count from "./Hook/Count"
 
 function App() {
   return (
     <div className="App">
-        <Cards/>
+        <Count/>
         
     </div>
     
